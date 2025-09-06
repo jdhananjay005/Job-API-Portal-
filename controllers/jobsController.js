e6c1e6c8d79ca2a85bff194c790dfc5393747578
Dhananjay <jdhananjay005@gmail.com>
2025-09-07 04:15:26 +0530

    pagination functionality has implemented

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -56,11 +56,21 @@ export const getAllJobsController = async (req, res, next) => {
       queryResult = queryResult.sort("-position");
     }
 
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit; // Remember this formula for future.
+    queryResult = queryResult.skip(skip).limit(limit);
+    // jobs count
+    const totalJobs = await jobModel.countDocuments(queryResult);
+    const numOfPage = Math.ceil(totalJobs / limit);
+
     const jobs = await queryResult;
 
     res.status(200).json({
       totalJobs: jobs.length,
       jobs,
+      numOfPage,
     });
   } catch (error) {
     next(error);
